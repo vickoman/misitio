3b99d4b69bf97936bbbb1ed07bab10cc2bc1db38
victor manuel <vicmandlagasca@gmail.com>
2014-02-10 23:43:44 -0500

    Boton de volver

--- misitio/settings.py
+++ misitio/settings.py
@@ -65,15 +65,13 @@ DATABASES = {
            'read_default_file' :  os.path.join(BASE_DIR, 'my.cnf'),
         },
     }
-}
-#DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
-#}
-
-
+#
+#    
+}
 
 
 # Internationalization
