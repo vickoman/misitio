e1910f164bca45863597bf5af4ed2c21724d7445
victor manuel <vicmandlagasca@gmail.com>
2014-02-11 15:26:32 -0500

    Setting 2

--- misitio/settings.py
+++ misitio/settings.py
@@ -59,19 +59,20 @@ WSGI_APPLICATION = 'misitio.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'OPTIONS': {
-#           'read_default_file' :  os.path.join(BASE_DIR, 'my.cnf'),
-#        },
-#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-
-    
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+           'read_default_file' :  os.path.join(BASE_DIR, 'my.cnf'),
+        },
+#    }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
 }
+
+
+
 
 
 # Internationalization
