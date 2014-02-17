d8bb6fc6fb7a3834db60c8878cabade416aad91a
victor manuel <vicmandlagasca@gmail.com>
2014-02-17 14:38:51 -0500

    Estilo de botones ocultos

--- misitio/settings.py
+++ misitio/settings.py
@@ -59,18 +59,16 @@ WSGI_APPLICATION = 'misitio.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'OPTIONS': {
-            'read_default_file' :  os.path.join(BASE_DIR, 'my.cnf'),
-         },
+ #    'default': {
+ #        'ENGINE': 'django.db.backends.mysql',
+ #        'OPTIONS': {
+ #           'read_default_file' :  os.path.join(BASE_DIR, 'my.cnf'),
+ #        },
+ #   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#
-#    
 }
 
 
